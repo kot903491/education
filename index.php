ad2676b73af766d5b8ac070f10089ce862f21206
kot903491 <kot903491@gmail.com>
2017-10-10 00:24:30 +0500

    поправил шаблон

--- index.php
+++ index.php
@@ -8,29 +8,47 @@
 $title = "Первое ДЗ";
 $h1="Первое задание";
 $year = date(Y);
+
 echo "
-<!doctype html>
-<html>
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml'>
 <head>
-    <meta charset='utf-8'>
-    
-    <title>". $title . "</title>
+	<meta http-equiv='content-type' content='text/html;charset=utf-8' />
+	<meta name='author' content='Luka Cvrk (www.solucija.com)' />
+	<link rel='stylesheet' href='css/main.css' type='text/css' />
+	<title>$title</title>
 </head>
 <body>
-    <header>
-        <h1>$h1</h1>
-    </header>
-    <div id='content'>
+	<div id='content'>
+		<h1>$h1</h1>
+
+		<ul id='menu'>
+			<li><a href='#'>home</a></li>
+			<li><a href='#'>archive</a></li>
+			<li><a href='#'>contact</a></li>
+		</ul>
+
+		<div class='post'>
+			<div class='details'>
+				<h2><a href='#'>Ответы выделенны жирным</a></h2>
+				<p class='info'>posted 3 hours ago in <a href='#'>general</a></p>
+
+			</div>
+			<div class='body'>
     \$a = 5;<br>
     \$b = '05';<br>
     var_dump(\$a == \$b);         // Почему true? <b>Потому, что \$b присвоился тип integer</b><br> 
     var_dump((int)'012345');     // Почему 12345? <b>Потому, что тип integer - числовой, а число не может начинаться с 0</b><br>
     var_dump((float)123.0 === (int)123.0); // Почему false? <b>Разные типы данных</b><br>
-    var_dump((int)0 === (int)'hello, world'); // Почему true? <b>В числовом типе нельзя записать слова, любой символ будет равен 0</b> <br>      
-<br><br><br>
-<h2>Меняем значения местами</h2>";
-$a = 5;
-$b = 12;
+    var_dump((int)0 === (int)'hello, world'); // Почему true? <b>В числовом типе нельзя записать слова, любой символ будет равен 0</b> <br>
+			</div>
+			<div class='x'></div>
+		</div>
+
+		<div class='col'>
+			<h3><a href='#'>Меняем значения переменных местами</a></h3>";
+$a = 4;
+$b = 15;
 echo "
 \$a = $a;<br>
 \$b = $b;<br>
@@ -45,12 +63,22 @@ echo "
 \$a-=\$b;<br>
 \$a = $a;<br>
 \$b = $b;
-    </div>
-<br><br><br><br><br><br>
-<footer>
-    &copy $year
-</footer>
+			<p>&not; <a href='#'>read more</a></p>
+		</div>
+		<div class='col'>
+			<h3><a href='#'>Maecenas iaculis leo</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href='#'>read more</a></p>
+		</div>
+		<div class='col last'>
+			<h3><a href='#'>Quisque consectetur odio</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href='#'>read more</a></p>
+		</div>
 
+		<div id='footer'>
+			<p>Copyright &copy; <em>minimalistica</em> &middot; Design: Luka Cvrk, <a href='http://www.solucija.com/' title='Free CSS Templates'>Solucija</a> $year</p>
+		</div>
+	</div>
 </body>
 </html>";
-
